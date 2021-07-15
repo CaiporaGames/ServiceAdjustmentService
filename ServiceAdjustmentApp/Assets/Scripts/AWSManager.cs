a699e157deac115484ddd2a90e478eaa76a7bf88
Caipora Games <73959333+CaiporaGames@users.noreply.github.com>
2021-07-15 19:06:05 -0300

    First Commit
    I download the necessary files, aws sdk for unity.

---
+++ ServiceAdjustmentApp/Assets/Scripts/AWSManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Amazon;
+public class AWSManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        UnityInitializer.AttachToGameObject(this.gameObject);
+    }
+}
